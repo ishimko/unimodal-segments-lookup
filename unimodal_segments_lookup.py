369f8d6c0e2febbf1258ade993dbcdcf36ef4c7c
vanashimko <vana.shimko@gmail.com>
2016-11-29 14:57:04 +0300

    Fix interval boundary exceed

--- unimodal_segments_lookup.py
+++ unimodal_segments_lookup.py
@@ -11,27 +11,32 @@ def get_first_point_where(target_function, current_point, condition_function, de
     while (not condition_function(target_function, current_point)) and (current_point <= interval_end):
         current_point += i * delta
         i += 1
-    return current_point
+    return current_point <= interval_end, current_point
 
 
 def get_unimodal_segments(target_function, interval_begin, interval_end, min_extremum_delta):
-    current_point = interval_begin
     result = []
-    delta = min_extremum_delta / 5
+    delta = min_extremum_delta / 3
 
     get_first_decreasing_point = lambda x: get_first_point_where(target_function, x, is_decreasing_at_point, delta,
                                                                  interval_end)
     get_first_increasing_point = lambda x: get_first_point_where(target_function, x, is_increasing_at_point, delta,
                                                                  interval_end)
 
+    current_interval_end = interval_begin
+    is_in_interval = True
     if is_decreasing_at_point(target_function, interval_begin):
-        current_point = get_first_increasing_point(interval_begin)
-        result.append((interval_begin, current_point))
+        is_in_interval, current_interval_end = get_first_increasing_point(interval_begin)
+        result.append((interval_begin, current_interval_end))
 
-    while current_point <= interval_end:
-        current_interval_begin = get_first_decreasing_point(current_point)
-        current_interval_end = get_first_increasing_point(current_interval_begin)
-        result.append((current_interval_begin, current_interval_end))
-        current_point = current_interval_end
+    end_iteration = not is_in_interval
+    while not end_iteration:
+        is_in_interval, current_interval_begin = get_first_decreasing_point(current_interval_end)
+        if is_in_interval:
+            is_in_interval, current_interval_end = get_first_increasing_point(current_interval_begin)
+            if not is_in_interval:
+                current_interval_end = interval_end
+                end_iteration = True
+            result.append((current_interval_begin, current_interval_end))
 
     return result
